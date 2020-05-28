69f34786f620d2456e1b6cda18026c13e44264e5
greek1821 <52238394+greek1821@users.noreply.github.com>
2020-05-28 14:02:01 +0300

    Create IntSO.cs

---
+++ IntSO.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+[CreateAssetMenu(fileName = "Integer", menuName = "ScriptableObjects/Variables/Integer")]
+public class IntSO : ScriptableObject
+{
+    public string Name;
+    public int Integer;
+}
